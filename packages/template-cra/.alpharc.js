01a33486f74a9b9553fb13c9b12dfc0c82da2168
fuyuan <yoranfu@gmail.com>
2018-06-14 19:53:43 +0800

    add create-react-app template

---
+++ packages/template-cra/.alpharc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // supported options in detail:
+  // https://github.com/yoranfu/alpha-scripts/blob/master/README.md
+  eslintrc: false,
+  babelrc: true,
+  // modules: ['shared'],
+  // proxy: {},
+  // alias: {},
+  mockOptions: {
+    path: 'mock',
+  },
+  // openPathname: '/homepage',
+  // theme: {}
+};
